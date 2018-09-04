f6ccc43c40ef79d465dd5f3645b067b807fdcbba
osmarks <osmarks@protonmail.com>
2018-09-04 14:55:20 +0100

    Fix the regexes.

--- server/dragcave.js
+++ server/dragcave.js
@@ -1,15 +1,21 @@
 const fetch = require("node-fetch");
 const { JSDOM } = require("jsdom");
 
-function getCapture(text, regex) {
+function getCapture(text, regex, def) {
     const result = text.match(regex);
-    if (result === null || result === undefined) { throw new Error("Text did not match regex."); }
+    if (result === null || result === undefined) { 
+        if (def === undefined) {
+            throw new Error("Text did not match regex.");
+        } else {
+            return def;
+        }
+    }
     else { return result[1]; }
 }
 
 // Parses a numeric counter somewhere in the text (like "Overall views: 123,456,789")
 function parseCounter(text, label) {
-    const re = new RegExp(".*" + label + ": *([0-9,]+)");
+    const re = new RegExp(label + ": *([0-9,]+)");
     return parseInt(getCapture(text, re).replace(",", ""));
 }
 
@@ -22,18 +28,22 @@ function infoFromHtml(html) {
     // First, just strip all the actual HTML & get text
     const text = JSDOM.fragment(html).textContent;
 
-    if (/.*will die if it doesn/.exec(text) === null) { return { type: "not growing" }; }
+    if (/will die if it doesn/.exec(text) === null) { return { type: "not growing" }; }
     
     let sick = false;
-    if (/.*shell of the egg seems soft,/.exec(text) !== null) { sick = true }
-    if (/.*hatchling seems sick/.exec(text) !== null) { sick = true }
+    // Check for text indicating sickness.
+    if (/shell of the egg seems soft,/.exec(text) !== null) { sick = true }
+    if (/hatchling seems sick/.exec(text) !== null) { sick = true }
 
+    // Parse all view-y counters
     const views = parseCounter(text, "Overall views");
     const uniqueViews = parseCounter(text, "Unique views");
     const clicks = parseCounter(text, "Clicks");
-    const timeResults = /.*in: ([0-9]+) days (and ([0-9]+) hours)?/.exec(text);
-    const days = parseInt(timeResults[1]), hours = parseInt(timeResults[3]);
-    const type = getCapture(text, /.*Viewing (Hatchling|Egg)/)
+    
+    // Try and parse remaining-time bits
+    const days = parseInt(getCapture(text, /in: ([0-9]+) day/, 0));
+    const hours = parseInt(getCapture(text, /and ([0-9]+) hour/, 0));
+    const type = getCapture(text, /Viewing (Hatchling|Egg)/)
 
     if (isNaN(days) || isNaN(hours)) { throw new Error("Either days or hours are now invalid; has DC's format changed?"); }
 
